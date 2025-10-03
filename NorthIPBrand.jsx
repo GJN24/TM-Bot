ce5e21e2b15211f61cd5e2096e07b5ae992ee559
GJN24 <geoffrey.north@northip.com>
2025-10-03 16:19:15 -0400

    Delete NorthIPBrand.jsx

--- NorthIPBrand.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function NorthIPBrand() {
-  return (
-    <div style={{fontWeight:'800', fontSize:18}}>
-      <span style={{color:'#D0021B'}}>North</span>
-      <span style={{color:'#000'}}> IP</span>
-    </div>
-  );
-}
